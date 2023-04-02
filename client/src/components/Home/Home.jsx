f989f7bd685a6cbebd9067e577c872c9bd6e6a57
Hui Cong <congegege@gmail.com>
2023-04-02 01:37:16 -0400

    add few get endpoints and fetch random recipe

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react"
+
 const Home = () =>{
-    return <>Home</>
+    const [randomRecipe,setRandomRecipe] = useState(null);
+    useEffect(()=>{
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+        .then(res=>res.json())
+        .then(resData=>setRandomRecipe(resData.drinks[0]))
+    },[])
+    console.log(randomRecipe)
+    
+    return (
+        <>
+    {randomRecipe &&<div>
+    <div>You might Like</div>
+    <img src={randomRecipe.strDrinkThumb
+}/>
+    </div>}
+    </>
+    )
 }
 export default Home
